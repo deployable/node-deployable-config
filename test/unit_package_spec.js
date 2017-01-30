2bc5058d1d34a7c2e4ca28a3c88de386de2daec5
Matt Hoyle <code@deployable.co>
2017-01-30 05:22:11 +0000

    add entrypoint require function for easier use

--- test/unit_package_spec.js
+++ test/unit_package_spec.js
@@ -7,11 +7,15 @@ describe('Unit::deployable-config', function(){
 
     let { Config, ConfigError, VERSION } = require('../')
 
-    it('should do load module from package.json entry point', function(){
+    it('should have a main export function', function(){
+      expect( require('../') ).to.be.a.function
+    })
+
+    it('should load the Config class from package.json entry point', function(){
       expect( Config ).to.be.ok
     })
 
-    it('should do load module from package.json entry point', function(){
+    it('should do load the ConfigError class from package.json entry point', function(){
       expect( ConfigError ).to.be.ok
     })
 
